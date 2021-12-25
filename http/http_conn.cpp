132a0266d2b801776a98174a416420d40ca7b2b1
WangQianlinux <1498953301@qq.com>
2021-12-25 18:35:01 +0200

    2021-12-26 1 完成了http_conn.cpp 并梳理了整个服务器运行的流程框架

--- http/http_conn.cpp
+++ http/http_conn.cpp
@@ -275,3 +275,201 @@ http_conn::HTTP_CODE http_conn::process_read(){
 
 
 //得到一个完整的HTTP请求时，需要分析这个请求的具体内容。
+
+http_conn::HTTP_CODE http_conn::do_request(){
+    //把doc_root复制到m_real_file指向的地方，准备作为目标文件的地址
+    strcpy(m_real_file,doc_root);
+    int len = strlen(doc_root);
+    //在doc_root后面跟着复制m_url
+    strncpy(m_real_file+len,m_url,FILENAME_LEN-len-1);
+    //m_file_stat中存储目标文件的一些属性，比如大小、是否为目录等
+    if(stat(m_real_file,&m_file_stat)<0){
+	return NO_RESOURCE;
+    }
+    //st_mode判断当前用户是否有读目标文件的权限
+    if(!(m_file_stat.st_mode&S_IROTH)){
+	return FORBIDDEN_REQUEST;
+    }
+    //S_ISDIR判断目标文件是否为目录
+    if(S_ISDIR(m_file_stat.st_mode)){
+	return BAD_REQUSET;
+    }
+    //这里将目标文件通过mmap映射到内存的一块空间...why不直接read然后发socket？或者用sendfile直接把文件往socket写。
+    int fd = open(m_real_file,O_RDONLY);
+    m_file_address = (char*)mmap(0,m_file_stat.st_size,PORT_READ,MAP_PRIVATE,fd,0);
+    close(fd);
+    return FILE_REQUEST;
+}
+
+//对内存映射区进行munmap操作，应该是释放内存。
+void http_conn::unmap(){
+    if(m_file_address){
+	munmap(m_file_address,m_file_stat.st_size);
+	m_file_address = 0;
+   }
+}
+
+//写HTTP响应
+bool http_conn::write(){
+    int temp = 0;
+    int bytes_have_send = 0;//已经发送了多少字节。
+    int bytes_to_send = m_write_idx;//还剩多少字节没发送。
+    //什么意思？
+    if(bytes_to_send == 0){
+	modfd(epollfd,m_sockfd,EPOLLIN);
+	init();
+	return true;
+    }
+    while(1){
+	//writev是集中写，把多块内存所在的地方统一写到目标fd里。前面的do_requset已经把目标文件写到一块内存里了。响应的响应行、头部字段和空行在另一块内存中。
+	temp = writev(m_sockfd,m_iv,m_iv_count);
+	//如果writev失败，说明集中写失败。why？可能是TCP写缓冲没有空间。这里要重新注册EPOLLOUT事件，因为采用了ET模式，只会返回一次。
+	if(temp<=-1){
+	    if(errno == EAGAIN){
+		modfd(m_epollfd,m_sockfd,EPOLLOUT);
+		return true;
+	    }
+	unmap();
+	return false;
+	}
+	bytes_to_send -= temp;
+	bytes_have_send += temp;
+	if(bytes_to_send<bytes_have_send){
+	    //发送HTTP成功？
+	    unmap();
+	    //如果是长连接，说明这次http请求处理结束后不close fd，
+	    if(m_linger){
+		init();
+		modfd(m_epollfd,m_sockfd,EPOLLIN);
+		return true;
+	    }
+	    else{
+		modfd(m_epollfd,m_sockfd,EPOLLIN);
+		return false;
+	    }
+	}
+    }
+}
+
+//往写缓冲中写入待发送的数据
+bool http_conn::add_response(const char* format,---){
+    if(m_write_idx>=WRITE_BUFFER_SIZE){
+	return false;
+    }
+    va_list arg_list;
+    va_start(arg_list,format);
+    int len = vsnprintf(m_write_buf+m_write_idx,WRITE_BUFFER_SIZE-1-m_write_idx,format,arg_list);
+    if(len>=(WRITE_BUFFER_SIZE-1-m_write_idx)){
+	return false;
+    }
+    m_write_idx += len;
+    va_end(arg_list);
+    return true;
+}
+
+
+//添加状态行
+bool http_conn:;add_status_line(int status,const char* title){
+    return add_response("%s %d %s\r\n","HTTP/1.1",status,title);
+}
+
+//添加头部字段
+bool http_conn::add_headers(int content_len){
+    add_content_length(content_len);
+    add_linger();
+    add_blank_line();
+}
+
+bool http_conn::add_content_length(int content_len){
+    return add_response("Content-Length:%d\r\n",content_len);
+}
+
+bool http_conn::add_linger(){
+    return add_response("Connection: %s\r\n",(m_linger==true)?"keep-live":"close");
+}
+
+bool http_conn::add_blank_line(){
+    return add_response("%s","\r\n");
+}
+
+bool http_conn::add_content(const char* content){
+    return add_response("%s",content);
+}
+
+//根据服务器处理HTTP请求的结果，决定返回给客户端的内容
+bool http_conn::process_write(HTTP_CODE ret){
+    switch(ret){
+	case INTERNAL_ERROR:{
+	    add_status_line(500,error_500_title);
+            add_headers(strlen(error_500_form));
+            if(!add_content(error_500_form)){
+                return false;
+            }
+	    break;
+	}
+	case BAD_REQUEST:{
+	    add_status_line(400,error_400_title);
+            add_headers(strlen(error_400_form));
+            if(!add_content(error_400_form)){
+                return false;
+            }
+	    break;
+	}
+	case NO_RESOURCE:{
+	    add_status_line(404,error_404_title);
+            add_headers(strlen(error_404_form));
+            if(!add_content(error_404_form)){
+                return false;
+            }
+	    break;
+	}
+	case FORBIDDEN_REQUEST:{
+	    add_status_line(403,error_403_title);
+	    add_headers(strlen(error_403_form));
+	    if(!add_content(error_403_form)){
+		return false;
+	    }
+	    break;
+	}
+	case FILE_REQUEST:{
+	    add_status_line(200,ok_200_title);
+	    if(m_file_stat.st_size != 0){
+		add_headers(m_file_stat.st_size);
+		m_iv[0],iov_base = m_write_buf;
+		m_iv[0].iov_len = m_write_idx;
+		m_iv[1].iov_base = m_file_address;
+		m_iv[1].iov_len = m_file_stat.st_size;
+		m_iv_count = 2;
+		return true;
+	    }
+	    else{
+		const char* ok_string = "<html><body></body></html>";
+		add_headers(strlen(ok_string));\
+		if(!add_content(ok_string)){
+		    return false;
+		}
+	    }
+	}
+	default:{
+	    return false;
+	}
+    }
+    m_iv[0].iov_base = m_write_buf;
+    m_iv[0].iov_len = m_write_idx;
+    m_iv_count = 1;
+    return true;
+}
+
+//线程池中的工作线程调用，这是处理HTTP的入口函数
+void http_conn::process(){
+    HTTP_CODE read_ret = process_read();
+    if(read_ret == NO_REQUEST){
+	modfd(m_epollfd,m_sockfd,EPOLLIN);
+   	return;
+     }
+    bool write_ret = process_write(read_ret);
+    if(!write_ret){
+	close_conn();
+    }
+    modfd(m_epollfd,m_sockfd,EPOLLOUT);
+}
